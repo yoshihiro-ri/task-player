5566a1050d0e6a856409a6bc0f2304fc2047a9ac
yoshihiro-ri <ri.dev1027@gmail.com>
2025-01-11 17:34:10 +0900

    first commit

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function HomePage() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home - Nextron (with-tailwindcss)</title>
+      </Head>
+    </React.Fragment>
+  )
+}
