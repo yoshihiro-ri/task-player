418826e61b0cafa6c576a06fa60618bedfd51e17
yoshihiro-ri <ri.dev1027@gmail.com>
2025-02-04 00:44:30 +0900

    ウィンドウ縮小アニメーション

--- main/helpers/update-bounds.ts
+++ main/helpers/update-bounds.ts
@@ -1,29 +1,42 @@
-import { screen, BrowserWindow } from "electron";
+import { screen,BrowserWindow } from "electron";
+import { start } from "repl";
 
-const activeWindowHeight = 500;
-const inactiveWindowHeight = 80;
-
-export const updateTaskPlayersBoundsOpened = (
+const animateWindowSize = async (
   window: BrowserWindow,
+  startTop: number,
+  endTop: number,
+  startWindowHeight: number,
+  endWindowHeight: number,
+  duration: number = 100
 ) => {
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  const steps = 30;
+  const interval = duration / steps;
+
+  for (let i = 0; i <= steps; i++) {
+    const progress = i / steps;
+    const currentTop = startTop - (startTop - endTop) * progress +38;
+    const currentWindowHeight = startWindowHeight - (startWindowHeight - endWindowHeight) * progress;
 
     window.setBounds({
-      x: 0,
-      y: height - activeWindowHeight + 38,
-      width: width,
-      height: activeWindowHeight,
+      y: currentTop,
+      height: Math.round(currentWindowHeight),
     });
 
+    await new Promise((resolve) => setTimeout(resolve, interval));
+  }
 };
 
-export const updateTaskPlayersBoundsClosed = (window: BrowserWindow) => {
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+// export const updateTaskPlayersBoundsOpened = async (window: BrowserWindow) => {
+//   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+//   await animateWindowSize(window, height, 500);
+// };
 
-  window.setBounds({
-    x: 0,
-    y: height - inactiveWindowHeight + 38,
-    width: width,
-    height: inactiveWindowHeight,
-  });
+export const updateTaskPlayersBoundsClosed = async (window: BrowserWindow) => {
+  const { height:screenHeight } = screen.getPrimaryDisplay().workAreaSize;
+  const startWindowHeight = 500
+  const startTop = screenHeight  - startWindowHeight
+  const endWindowHeight = 80
+  const endTop = screenHeight  - 80
+  await animateWindowSize(window, startTop, endTop,startWindowHeight,endWindowHeight);
 };
